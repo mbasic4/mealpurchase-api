75c029cf3524cad3b3c62d3961efd4ef726f851b
mbasic <gnomnagljivi@gmail.com>
2022-04-04 21:48:12 +0200

    implement pagination

--- src/routes/meals/meals.controller.ts
+++ src/routes/meals/meals.controller.ts
@@ -4,6 +4,6 @@ import mealsService from './meals.service'
 export const mealsController = express.Router()
 
 mealsController.get('/v1/meals', async function (req, res) {
-  const mealsData = mealsService.getList()
+  const mealsData = mealsService.getList(req.query)
   res.send(mealsData)
 })
--- src/routes/meals/meals.service.ts
+++ src/routes/meals/meals.service.ts
@@ -1,7 +1,47 @@
 import mealsData from '../../mealsData.json'
 
-function getList () {
-  return mealsData.meals
+interface Meal {
+  id: string
+  title: string
+  starter: string
+  desert: string
+  price: number
+  labels: Array<string>
+  img: string
+  drinks: Array<Drink>
+}
+
+interface Drink {
+  id: string
+  title: string
+  price: number
+}
+
+function getList (query) {
+  const limit = parseInt(query.limit)
+  const page = parseInt(query.page)
+  const offset = (page - 1) * limit
+
+  let data = { data: mealsData.meals } as { data: Array<Meal>, totalCount: number }
+
+  if (query.mealIds) {
+    const meals = data.data.filter(meal => query.mealIds.includes(meal.id))
+    return { data: meals, totalCount: meals.length }
+  }
+
+  if (query.mealCategoryId) {
+    const filteredData = data.data.filter(meal => meal.labels.includes(query.mealCategoryId))
+    data = { data: filteredData, totalCount: filteredData.length }
+    return data
+  }
+
+  if (limit) {
+    const mealsDataCopy = [ ...mealsData.meals ]
+    data = { data: mealsDataCopy.splice(offset, limit), totalCount: mealsData.meals.length }
+    return data
+  }
+
+  return { data: mealsData.meals, totalCount: mealsData.meals.length }
 }
 
 export default {
